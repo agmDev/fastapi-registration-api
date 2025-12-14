aa4de42ae40b29d5e67652678769674d1674b0fd
agmDev <dumont.antoine@orange.fr>
2025-12-14 01:08:01 +0100

    forgot to add the main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,37 @@
 from fastapi import FastAPI
+import logging
+from contextlib import asynccontextmanager
+
+from app.settings import settings
 from app.api.router.healthcheck import router as healthcheck_router
 from app.api.router.users import router as user_router
 from app.api.router.activation import router as activation_router
+from app.infrastructure.init_db import init_database
 
-def create_app() -> FastAPI:
-    app = FastAPI(title="User Registration API", version="0.1.0")
 
-    routers = healthcheck_router, user_router, activation_router
-    for router in routers:
-        app.include_router(router)
+logger = logging.getLogger(__name__)
 
-    return app
 
-app = create_app()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("starting application")
+    try:
+        await init_database()
+
+        routers = healthcheck_router, user_router, activation_router
+        for router in routers:
+            app.include_router(router)
+        logger.info("application started succesfully")
+    except:
+        logger.error(f"Failed to start application:", exc_info=True)
+        raise
+
+    yield
+
+
+app = FastAPI(
+    title=settings.app_name,
+    description=settings.description,
+    version=settings.version,
+    lifespan=lifespan
+)
